207366485faacd67238be329e640221ae241bbb7
Quaris <quaris314@gmail.com>
2026-06-08 18:49:38 -0500

    Technical: Create gui component for displaying ship systems' repair levels

--- data/missions/common/common_signals.py
+++ data/missions/common/common_signals.py
@@ -7,3 +7,14 @@ def signal_after_player_ship_destroyed():
 
 def signal_comms_property_list_box_update_ship_to_ship(player_ship_id):
     return f"comms_property_list_box_update_ship_to_ship_{player_ship_id}"
+
+# system node repair changes
+
+def signal_system_nodes_repair_changed(ship_id):
+    return f"system_nodes_repair_changed_{ship_id}"
+
+def signal_heat_pool_system_nodes_repair_changed(ship_id):
+    return f"system_group_repair_changed_{ship_id}"
+
+def signal_all_system_nodes_repair_changed(ship_id):
+    return f"all_system_nodes_repair_changed_{ship_id}"
