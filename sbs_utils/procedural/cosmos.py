8210342520058e8147ceef16f39ed4d9c5cd03da
Quaris <quaris314@gmail.com>
2026-07-21 03:18:28 -0500

    Restart game w/o restart script: When creating a new simulation, delete sbs_utils's copies of space object and grid object data

--- sbs_utils/procedural/cosmos.py
+++ sbs_utils/procedural/cosmos.py
@@ -1,3 +1,6 @@
+import sbs
+from sbs_utils.agent import Agent
+from sbs_utils.procedural.query import is_grid_object_id, is_space_object_id
 from ..helpers import FrameContext
 
 """
@@ -6,9 +9,14 @@ Functions to the engine
 
 def sim_create() -> None:
     """ Creates a new simulation
-    """    
+    """
+    grid_object_ids = [agent_id for agent_id in Agent.all if is_grid_object_id(agent_id)]
+    space_object_ids = [agent_id for agent_id in Agent.all if is_space_object_id(agent_id)]
     FrameContext.context.sbs.create_new_sim()
-    
+    for grid_object_id in grid_object_ids:
+        Agent.remove_id(grid_object_id)
+    for space_object_id in space_object_ids:
+        Agent.remove_id(space_object_id)
 
 def sim_pause() -> None:
     """pauses the simulation
